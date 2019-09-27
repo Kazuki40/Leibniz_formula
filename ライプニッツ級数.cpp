9fd790955a62df46888945c3531828bcd85cf3f3
Miyazaki <54506893+MiyazakiKazuki0823@users.noreply.github.com>
2019-09-27 18:40:23 +0900

    Create ライプニッツ級数.cpp

---
+++ ライプニッツ級数.cpp
@@ -0,0 +1,21 @@
+﻿//ライプニッツ級数
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+//宣言
+long long int n, i;
+double fx;
+
+int main() {
+	cout << "繰り返し回数を入力\n";
+	cin >> n;
+	for (i = 0; i <= n; i++) {
+		fx += (pow(-1, i)) / (2 * i + 1);
+	}
+	fx = fx * 4;
+	cout << "答えは" << scientific << fx << endl;
+	return 0;
+}
